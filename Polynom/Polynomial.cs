44d8850fa1776f84a7dc52ddede75cd0ad9a8d11
RuslanMudruj <32366107+RuslanMudruj@users.noreply.github.com>
2021-10-05 23:33:10 +0300

    Add files via upload
    Це майже готовий варіант, але я ще дороблю

---
+++ Polynom/Polynomial.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polynom
+{
+    class Polynomial
+    {
+        int[] koef; //массив коэф-ов
+        int step; //значение степени полинома
+
+        public Polynomial(int[] k, int s)
+        {
+            koef = k;
+            step = s;
+        }
+
+        //сложение полиномов
+        public static Polynomial max_pol(Polynomial A, Polynomial B)
+        {
+            if (A.step > B.step)
+            {
+                return A;
+            }
+            else
+                return B;
+        }
+        public static Polynomial operator +(Polynomial A, Polynomial B)
+        {
+            int D1 = max_pol(A, B).step;
+            int[] M1 = new int[D1 + 1];
+            Polynomial C = new Polynomial(M1, D1);
+            if (A.step > B.step)
+            {
+                for (int i = 0; i < B.koef.Length; i++)
+                {
+                    C.koef[i] = A.koef[i] + B.koef[i];
+                }
+                for (int j = B.koef.Length; j < M1.Length; j++)
+                {
+                    C.koef[j] = max_pol(A, B).koef[j];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < A.koef.Length; i++)
+                {
+                    C.koef[i] = A.koef[i] + B.koef[i];
+                }
+                for (int j = A.koef.Length; j < M1.Length; j++)
+                {
+                    C.koef[j] = max_pol(A, B).koef[j];
+                }
+            }
+
+            return C;
+        }
+
+        //вычитание полиномов
+        public static Polynomial operator -(Polynomial A, Polynomial B)
+        {
+            int D1 = max_pol(A, B).step;
+            int[] M1 = new int[D1 + 1];
+            Polynomial C = new Polynomial(M1, D1);
+            if (A.step > B.step)
+            {
+                for (int i = 0; i < B.koef.Length; i++)
+                {
+                    C.koef[i] = A.koef[i] - B.koef[i];
+                }
+                for (int j = B.koef.Length; j < M1.Length; j++)
+                {
+                    C.koef[j] = max_pol(A, B).koef[j];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < A.koef.Length; i++)
+                {
+                    C.koef[i] = A.koef[i] - B.koef[i];
+                }
+                for (int j = A.koef.Length; j < M1.Length; j++)
+                {
+                    C.koef[j] = max_pol(A, B).koef[j];
+                }
+            }
+            return C;
+        }
+
+        //умножение полиномов
+        public static Polynomial operator *(Polynomial A, Polynomial B)
+        {
+            int D1 = A.step + B.step;
+            int[] M1 = new int[D1 + 1];
+            Polynomial C = new Polynomial(M1, D1);
+            for (int i = 0; i <= A.step; i++)
+            {
+                for (int j = 0; j <= B.step; j++)
+                {
+                    C.koef[i + j] += A.koef[i] * B.koef[j];
+                }
+            }
+            return C;
+            
+        }
+
+        //вывод полинома
+        public void show()
+        {
+            for (int i = step+1,j=0; i !=0; j++,i--)
+            {
+                Console.Write("+" + koef[j] + "x^" + (j));
+            }
+            //return 0;
+        }
+
+        public void  Parse(string S)
+        {
+            string[] SS = S.Split("+X");
+            this.koef = new int[SS.Length];
+            this.step = SS.Length - 1;
+            for (int i = 0; i < koef.Length; i++)
+                this.koef[i] = int.Parse(SS[i]);
+        }
+
+    }
+}
---
+++ Polynom/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Це майже готовий варінт завдання
+namespace Polynom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод первого многочлена
+            int step;
+            int[] koef;
+
+            Console.WriteLine("Enter matrix 1 polynom");
+            step = Convert.ToInt32(Console.ReadLine());
+
+            int Val = 0;
+           
+
+
+            koef = new int[step + 1];
+
+            Console.WriteLine("Enter matrix 1 polynom");
+            for (int i = 0; i < koef.Length; i++)
+            {
+                koef[i] = int.Parse(Console.ReadLine());
+            }
+            Polynomial A = new Polynomial(koef, step);
+
+            
+            int step2;
+            int[] koef2;
+
+            Console.WriteLine("Enter step 2 polynom");
+             step2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+             koef2 = new int [step2+1];
+
+             Console.WriteLine("Enter matrix 2 polynom");
+             for (int i = 0; i < koef2.Length; i++)
+             {
+                 koef2[i] = int.Parse(Console.ReadLine());
+             }
+             Polynomial B = new Polynomial(koef2, step2);
+
+             //вывод многочленов
+             Console.Write("A= ");
+             A.show();
+             Console.WriteLine("\n");
+             Console.Write("B= ");
+             B.show();
+
+             //вывод операций
+
+             Polynomial C = A + B;
+             Polynomial D = A - B;
+             Polynomial E = A * B;
+             Console.Write("\nA+B= ");
+             C.show();
+            Console.Write("\nA-B= ");
+            D.show();
+            Console.Write("\nA*B= ");
+            E.show();
+
+            Console.ReadLine();
+        }
+    }
+}
+
+            
+
+
